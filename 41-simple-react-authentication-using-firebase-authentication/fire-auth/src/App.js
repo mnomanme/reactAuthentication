409cfbcffaeead835d518c51bb64b8c52ec5f8a0
nomanme <abdullah.noman083@gmail.com>
2021-09-22 21:13:23 +0600

    compo add

--- 41-simple-react-authentication-using-firebase-authentication/fire-auth/src/App.js
+++ 41-simple-react-authentication-using-firebase-authentication/fire-auth/src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import FirebaseAuth from './components/FirebaseAuth/FirebaseAuth';
 
 function App() {
 	return (
 		<div className="App">
 			<h2>Firebase Authentication</h2>
+			<FirebaseAuth />
 		</div>
 	);
 }
