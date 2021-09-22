6a09c06009ffecd7aebf443722a645a76094109b
nomanme <abdullah.noman083@gmail.com>
2021-09-22 21:36:23 +0600

    sign in pop up show

--- 41-simple-react-authentication-using-firebase-authentication/fire-auth/src/components/FirebaseAuth/FirebaseAuth.js
+++ 41-simple-react-authentication-using-firebase-authentication/fire-auth/src/components/FirebaseAuth/FirebaseAuth.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { initializeApp } from 'firebase/app';
-import { GoogleAuthProvider } from 'firebase/auth';
-
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import firebaseConfig from '../../firebase.config';
 
 const FirebaseAuth = () => {
@@ -9,10 +8,41 @@ const FirebaseAuth = () => {
 
 	const provider = new GoogleAuthProvider();
 
+	const handleSignIn = () => {
+		console.log('sign in');
+
+		// Authenticate with Firebase using the Google provider object
+		const auth = getAuth();
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				console.log(result);
+				const { displayName, email, photoURL } = result.user;
+				console.log(displayName, email, photoURL);
+				// This gives you a Google Access Token. You can use it to access the Google API.
+				const credential = GoogleAuthProvider.credentialFromResult(result);
+				const token = credential.accessToken;
+				// The signed-in user info.
+				const user = result.user;
+				// ...
+			})
+			.catch((error) => {
+				// Handle Errors here.
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				// The email of the user's account used.
+				const email = error.email;
+				// The AuthCredential type that was used.
+				const credential = GoogleAuthProvider.credentialFromError(error);
+				// ...
+			});
+	};
+
 	return (
 		<div>
 			<h2>Auth</h2>
-			<button className="btn btn-outline-info btn-lg">Sign In</button>
+			<button onClick={handleSignIn} className="btn btn-outline-info btn-lg">
+				Sign In
+			</button>
 		</div>
 	);
 };
