237b5a21580b531cfba03710c47c5c8f7fa1ff96
nomanme <abdullah.noman083@gmail.com>
2021-09-27 16:06:56 +0600

    object add

--- 42-login-form-and-route-integration/fire-auth/src/components/AuthRegister/AuthRegister.js
+++ 42-login-form-and-route-integration/fire-auth/src/components/AuthRegister/AuthRegister.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const AuthRegister = (props) => {
 	// console.log(props);
@@ -7,29 +8,48 @@ const AuthRegister = (props) => {
 	const user = props.user;
 	const setUser = props.setUser;
 
-	const handleSubmit = () => {
-		console.log('submit');
+	const handleSubmit = (e) => {
+		// console.log('submit');
+		// console.log('form submit', user.email, user.password);
+
+		if (user.email && user.password) {
+			const auth = getAuth();
+			createUserWithEmailAndPassword(auth, user.email, user.password)
+				.then((res) => {
+					const newUserInfo = { ...user };
+					newUserInfo.error = '';
+					newUserInfo.success = true;
+					setUser(newUserInfo);
+				})
+				.catch((error) => {
+					const newUserInfo = { ...user };
+					newUserInfo.error = error.message;
+					newUserInfo.success = false;
+					setUser(newUserInfo);
+				});
+		}
+		e.preventDefault();
 	};
 
 	const handleBlur = (event) => {
-		console.log(event.target.value);
-		let isFormValid = true;
+		// console.log(event.target.value);
+		let isFieldValid = true;
 
 		if (event.target.name === 'email') {
 			// const isEmailValid = /\S+@\S+\.\S+/.test(event.target.value);
 			// console.log(isEmailValid);
-			isFormValid = /\S+@\S+\.\S+/.test(event.target.value);
+			isFieldValid = /\S+@\S+\.\S+/.test(event.target.value);
 		}
 		if (event.target.name === 'password') {
 			const isPasswordValid = event.target.value.length > 6;
 			// console.log(isPasswordValid);
 			const passHasNumber = /\d{1}/.test(event.target.value);
 			// console.log(isPasswordValid && passHasNumber);
-			isFormValid = isPasswordValid && passHasNumber;
+			isFieldValid = isPasswordValid && passHasNumber;
 		}
-		if (isFormValid) {
+		if (isFieldValid) {
 			const newUserInfo = { ...user };
-			console.log(newUserInfo);
+			// console.log(newUserInfo);
 			newUserInfo[event.target.name] = event.target.value;
 			setUser(newUserInfo);
 		}
@@ -43,14 +63,17 @@ const AuthRegister = (props) => {
 			<p>Password: {user.password}</p>
 			<br />
 			<form onSubmit={handleSubmit}>
-				<input onBlur={handleBlur} type="text" name="name" id="" placeholder="your name" />
+				<input onBlur={handleBlur} type="text" name="name" id="" placeholder="your name" className="form-control mx-auto w-25" />
 				<br />
-				<input onBlur={handleBlur} type="text" name="email" id="" placeholder="your email" required />
+				<input onBlur={handleBlur} type="text" name="email" id="" placeholder="your email" className="form-control mx-auto w-25" required />
 				<br />
-				<input onBlur={handleBlur} type="password" name="password" id="" placeholder="your password" required />
+				<input onBlur={handleBlur} type="password" name="password" id="" placeholder="your password" className="form-control mx-auto w-25" required />
 				<br />
-				<input type="submit" value="Submit" />
+				<input className="btn btn-info" type="submit" value="Submit" />
 			</form>
+			<br />
+			<p className="text-danger">{user.error}</p>
+			{user.success && <p className="text-success">User Created Successfully</p>}
 		</section>
 	);
 };
