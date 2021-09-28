6addd34f89db99e795b980b945862fcc0de64229
nomanme <abdullah.noman083@gmail.com>
2021-09-28 21:33:26 +0600

    auth recap setup

---
+++ 42.5-firebase-recap-and-practice/recap-auth/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+	return (
+		<section className="App">
+			<h2>Auth Recap</h2>
+		</section>
+	);
+};
+
+export default App;
---
+++ 42.5-firebase-recap-and-practice/recap-auth/src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyBWaPVOywrwE3RNKgJcNmp2UBG_eCcIVnI',
+	authDomain: 'recap-auth-test.firebaseapp.com',
+	projectId: 'recap-auth-test',
+	storageBucket: 'recap-auth-test.appspot.com',
+	messagingSenderId: '382354163240',
+	appId: '1:382354163240:web:628ce9d5b8e4f1f0cc971d',
+};
+
+export default firebaseConfig;
