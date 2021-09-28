43862b30b6076bc20bb111642bdace9f63f563ed
nomanme <abdullah.noman083@gmail.com>
2021-09-28 22:45:49 +0600

    github login

--- 42.5-firebase-recap-and-practice/recap-auth/src/Auth.js
+++ 42.5-firebase-recap-and-practice/recap-auth/src/Auth.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { initializeApp } from 'firebase/app';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithPopup, GithubAuthProvider } from 'firebase/auth';
 import { Button } from 'react-bootstrap';
 import firebaseConfig from './firebase.config';
 
 const Auth = () => {
 	const app = initializeApp(firebaseConfig);
 	const googleProvider = new GoogleAuthProvider();
-	console.log('app', app, 'googleProvider', googleProvider);
+	const githubProvider = new GithubAuthProvider();
+
+	console.log('app', app, 'googleProvider', googleProvider, 'githubProvider', githubProvider);
 
 	const [user, setUser] = useState({});
 
@@ -36,10 +38,41 @@ const Auth = () => {
 				console.log('error credential', credential);
 			});
 	};
+
+	const handleGitHubSignIn = () => {
+		console.log('github sign in');
+		const auth = getAuth();
+		signInWithPopup(auth, githubProvider)
+			.then((result) => {
+				const credential = GoogleAuthProvider.credentialFromResult(result);
+				const token = credential.accessToken;
+				const user = result.user;
+				setUser(user);
+				console.log('credential', credential);
+				console.log('token', token);
+				console.log('github user', user);
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				const email = error.email;
+				const credential = GoogleAuthProvider.credentialFromError(error);
+				console.log('errorCode', errorCode);
+				console.log('errorMessage', errorMessage);
+				console.log('error email', email);
+				console.log('error credential', credential);
+				console.log(error);
+			});
+	};
+
 	return (
 		<>
 			<Button onClick={handleGoogleSignIn} variant="success">
 				Google Sign In
+			</Button>
+			<br />
+			<Button onClick={handleGitHubSignIn} variant="info" className="my-2">
+				Github Sign In
 			</Button>
 			<h4>Email: {user.email}</h4>
 			<img src={user.photoURL} alt="" />
