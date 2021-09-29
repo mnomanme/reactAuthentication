1e94c0ab706af1a8765f1e5b3c5fe81314280d7e
nomanme <abdullah.noman083@gmail.com>
2021-09-30 00:17:13 +0600

    register done

---
+++ 43-shared-auth-and-protected-route/ema-john-auth/src/components/Login/AuthRegister.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { Container } from 'react-bootstrap';
+
+const AuthRegister = (props) => {
+	// console.log(props);
+	// console.log(props.user);
+	const [newUser, setNewUser] = useState(false);
+
+	const user = props.user;
+	const setUser = props.setUser;
+
+	const handleSubmit = (e) => {
+		// console.log('submit');
+		// console.log('form submit', user.email, user.password);
+
+		if (newUser && user.email && user.password) {
+			const auth = getAuth();
+			createUserWithEmailAndPassword(auth, user.email, user.password)
+				.then((res) => {
+					const newUserInfo = { ...user };
+					newUserInfo.error = '';
+					newUserInfo.success = true;
+					setUser(newUserInfo);
+					updateUserName(user.name);
+				})
+				.catch((error) => {
+					const newUserInfo = { ...user };
+					newUserInfo.error = error.message;
+					newUserInfo.success = false;
+					setUser(newUserInfo);
+				});
+		}
+
+		if (!newUser && user.email && user.password) {
+			const auth = getAuth();
+			signInWithEmailAndPassword(auth, user.email, user.password)
+				.then((res) => {
+					const newUserInfo = { ...user };
+					newUserInfo.error = '';
+					newUserInfo.success = true;
+					setUser(newUserInfo);
+					console.log('sign in user info', res.user);
+				})
+				.catch((error) => {
+					const newUserInfo = { ...user };
+					newUserInfo.error = error.message;
+					newUserInfo.success = false;
+					setUser(newUserInfo);
+				});
+		}
+		e.preventDefault();
+	};
+
+	const handleBlur = (event) => {
+		// console.log(event.target.value);
+		let isFieldValid = true;
+
+		if (event.target.name === 'email') {
+			isFieldValid = /\S+@\S+\.\S+/.test(event.target.value);
+			// const isEmailValid = /\S+@\S+\.\S+/.test(event.target.value);
+			// console.log(isEmailValid);
+		}
+		if (event.target.name === 'password') {
+			const isPasswordValid = event.target.value.length > 6;
+			const passHasNumber = /\d{1}/.test(event.target.value);
+			isFieldValid = isPasswordValid && passHasNumber;
+			// console.log(isPasswordValid);
+			// console.log(isPasswordValid && passHasNumber);
+		}
+		if (isFieldValid) {
+			const newUserInfo = { ...user };
+			newUserInfo[event.target.name] = event.target.value;
+			setUser(newUserInfo);
+			// console.log(newUserInfo);
+		}
+	};
+
+	const updateUserName = (name) => {
+		const auth = getAuth();
+		updateProfile(auth.currentUser, {
+			displayName: name,
+		})
+			.then(() => {
+				console.log('user name updated successfully');
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	return (
+		<>
+			<p>Name: {user.name}</p>
+			<p>Email: {user.email}</p>
+			<p>Password: {user.password}</p>
+			<br />
+			<input onChange={() => setNewUser(!newUser)} type="checkbox" name="newUser" id="" />
+			<label htmlFor="newUser">New User Sign Up</label>
+
+			<form onSubmit={handleSubmit}>
+				{newUser && <input onBlur={handleBlur} type="text" name="name" id="" placeholder="your name" className="form-control mx-auto w-25" />}
+				<br />
+				<input onBlur={handleBlur} type="text" name="email" id="" placeholder="your email" className="form-control mx-auto w-25" required />
+				<br />
+				<input onBlur={handleBlur} type="password" name="password" id="" placeholder="your password" className="form-control mx-auto w-25" required />
+				<br />
+				<input className="btn btn-info" type="submit" value={newUser ? 'Sign up' : 'Sign in'} />
+			</form>
+			<br />
+			<p className="text-danger">{user.error}</p>
+			{user.success && <p className="text-success">User {newUser ? 'Created' : 'Logged In'} Successfully</p>}
+		</>
+	);
+};
+
+export default AuthRegister;
