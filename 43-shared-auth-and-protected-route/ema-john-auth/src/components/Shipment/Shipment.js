b0111fd7c9346557abc88da69d912ac10931aa5e
nomanme <abdullah.noman083@gmail.com>
2021-09-30 00:18:32 +0600

    shipment add

---
+++ 43-shared-auth-and-protected-route/ema-john-auth/src/components/Shipment/Shipment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Shipment = () => {
+	return (
+		<Container>
+			<h1>This is Shipment</h1>
+		</Container>
+	);
+};
+
+export default Shipment;
