33d5428dcd7e2313f9545d1ba4457b971c4deaed
nomanme <abdullah.noman083@gmail.com>
2021-09-30 00:17:33 +0600

    config firebase

---
+++ 43-shared-auth-and-protected-route/ema-john-auth/src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyAUnPgmv9m__xFGrOldjjGDTOLDk0NOXqo',
+	authDomain: 'ema-john-auth-v4.firebaseapp.com',
+	projectId: 'ema-john-auth-v4',
+	storageBucket: 'ema-john-auth-v4.appspot.com',
+	messagingSenderId: '829538960669',
+	appId: '1:829538960669:web:46d24b2c8e94c5b185cdf4',
+};
+
+export default firebaseConfig;
