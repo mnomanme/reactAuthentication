b03f4b3a2a09a13be54f979f77f6d498bcf3bb17
nomanme <abdullah.noman083@gmail.com>
2021-10-02 11:48:10 +0600

    header done

--- 43-shared-auth-and-protected-route/ema-john-auth/src/components/Header/Header.js
+++ 43-shared-auth-and-protected-route/ema-john-auth/src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import logo from '../../resources/images/logo.png';
 import { Link } from 'react-router-dom';
+import { userContext } from '../Layouts/Layouts';
 
 const Header = () => {
+	const [loggedInUser, setLoggedInUser] = useContext(userContext);
 	return (
 		<div>
 			<Container className="header">
@@ -13,6 +15,9 @@ const Header = () => {
 					<Link to="/shop">Shop</Link>
 					<Link to="/review">Order Review</Link>
 					<Link to="/inventory">Manage Inventory</Link>
+					<Button onClick={() => setLoggedInUser()} variant="outline-warning " size="lg">
+						Sign Out
+					</Button>
 				</nav>
 			</Container>
 		</div>
