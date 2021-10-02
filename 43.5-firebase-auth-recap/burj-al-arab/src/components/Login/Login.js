f6f81e7e31116ba90cd29d4a6f5f44c9dd1267e3
nomanme <abdullah.noman083@gmail.com>
2021-10-02 15:21:54 +0600

    sign in update

--- 43.5-firebase-auth-recap/burj-al-arab/src/components/Login/Login.js
+++ 43.5-firebase-auth-recap/burj-al-arab/src/components/Login/Login.js
@@ -1,10 +1,39 @@
 import React, { useContext } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import firebaseConfig from './firebase.config';
 
 const Login = () => {
+	const handleGoogleSignIn = () => {
+		console.log('sign in google');
+
+		const app = initializeApp(firebaseConfig);
+		const googleProvider = new GoogleAuthProvider();
+
+		const auth = getAuth();
+		signInWithPopup(auth, googleProvider)
+			.then((result) => {
+				const credential = GoogleAuthProvider.credentialFromResult(result);
+				const token = credential.accessToken;
+				const user = result.user;
+				console.log(user);
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				const email = error.email;
+				const credential = GoogleAuthProvider.credentialFromError(error);
+			});
+	};
+
 	return (
-		<div>
+		<Container className="text-center">
 			<h1>This is Login</h1>
-		</div>
+			<Button onClick={handleGoogleSignIn} variant="outline-secondary">
+				Google Sign In
+			</Button>
+		</Container>
 	);
 };
 
