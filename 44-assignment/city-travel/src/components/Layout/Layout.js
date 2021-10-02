3ce22cf231ee417e769444a117679483eed44d6f
nomanme <abdullah.noman083@gmail.com>
2021-10-02 21:24:44 +0600

    pass layout

--- 44-assignment/city-travel/src/components/Layout/Layout.js
+++ 44-assignment/city-travel/src/components/Layout/Layout.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
+import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Blog from '../Blog/Blog';
 import Contact from '../Contact/Contact';
@@ -7,32 +8,42 @@ import Header from '../Header/Header';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
 import { NotMatch } from '../NotMatch/NotMatch';
+import PrivateRoute from '../PrivateRoute/PrivateRoute';
+
+export const userContext = createContext();
 
 const Layout = () => {
+	const [loggedInUser, setLoggedInUser] = useState({});
+
 	return (
-		<Router>
-			<Header />
-			<Switch>
-				<Route exact path="/home">
-					<Home />
-				</Route>
-				<Route path="/destination">
-					<Destination />
-				</Route>
-				<Route path="/blog">
-					<Blog />
-				</Route>
-				<Route path="/contact">
-					<Contact />
-				</Route>
-				<Route path="/login">
-					<Login />
-				</Route>
-				<Route exact path="*">
-					<NotMatch />
-				</Route>
-			</Switch>
-		</Router>
+		<userContext.Provider value={[loggedInUser, setLoggedInUser]}>
+			<Router>
+				<Container>
+					<h5 className="bg-info rounded text-center">email: {loggedInUser?.email}</h5>
+				</Container>
+				<Header />
+				<Switch>
+					<Route exact path="/home">
+						<Home />
+					</Route>
+					<PrivateRoute path="/destination">
+						<Destination />
+					</PrivateRoute>
+					<Route path="/blog">
+						<Blog />
+					</Route>
+					<Route path="/contact">
+						<Contact />
+					</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route exact path="*">
+						<NotMatch />
+					</Route>
+				</Switch>
+			</Router>
+		</userContext.Provider>
 	);
 };
 
