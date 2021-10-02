68eb8b9835943936065aacc952583417c5e4bdf5
nomanme <abdullah.noman083@gmail.com>
2021-10-02 11:48:38 +0600

    private route done

--- 43-shared-auth-and-protected-route/ema-john-auth/src/components/PrivateRoute/PrivateRoute.js
+++ 43-shared-auth-and-protected-route/ema-john-auth/src/components/PrivateRoute/PrivateRoute.js
@@ -10,7 +10,7 @@ const PrivateRoute = ({ children, ...rest }) => {
 		<Route
 			{...rest}
 			render={({ location }) =>
-				loggedInUser.email ? (
+				loggedInUser?.email ? (
 					children
 				) : (
 					<Redirect
