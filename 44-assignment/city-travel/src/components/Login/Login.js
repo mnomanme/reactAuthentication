9a618f1ce4238b914b7493783edc11446b18957e
nomanme <abdullah.noman083@gmail.com>
2021-10-02 21:23:25 +0600

    google login done

--- 44-assignment/city-travel/src/components/Login/Login.js
+++ 44-assignment/city-travel/src/components/Login/Login.js
@@ -1,10 +1,99 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
+import AuthRegister from './AuthRegister';
+import firebaseConfig from './firebase.config';
+import { userContext } from '../Layout/Layout';
 
 const Login = () => {
+	const app = initializeApp(firebaseConfig);
+	console.log(app);
+
+	const googleProvider = new GoogleAuthProvider();
+
+	const [user, setUser] = useState({
+		isSignedIn: false,
+		name: '',
+		email: '',
+		photoURL: '',
+	});
+
+	// context API
+	const [loggedInUser, setLoggedInUser] = useContext(userContext);
+
+	// google sign in user
+	const handleSignIn = () => {
+		console.log('sign in');
+
+		// Authenticate with Firebase using the Google provider object
+		const auth = getAuth();
+		signInWithPopup(auth, googleProvider)
+			.then((result) => {
+				console.log(result);
+				const { displayName, email, photoURL } = result.user;
+				console.log(displayName, email, photoURL);
+
+				const signedInUser = {
+					isSignedIn: true,
+					name: displayName,
+					email: email,
+					photoURL: photoURL,
+				};
+				setUser(signedInUser);
+				setLoggedInUser(signedInUser);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	// google sign out user
+	const handleSignOut = () => {
+		console.log('sign out');
+		const auth = getAuth();
+		signOut(auth)
+			.then(() => {
+				const signedOutUser = {
+					isSignedIn: false,
+					name: '',
+					email: '',
+					photoURL: '',
+					error: '',
+					success: false,
+				};
+				setUser(signedOutUser);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
 	return (
-		<div>
-			<h2>This is login</h2>
-		</div>
+		<section className="m-2 text-center">
+			{user?.isSignedIn ? (
+				<button onClick={handleSignOut} className="btn btn-outline-info btn-lg">
+					Google Sign Out
+				</button>
+			) : (
+				<button onClick={handleSignIn} className="btn btn-outline-info btn-lg">
+					Google Sign In
+				</button>
+			)}
+			<br />
+			{user?.isSignedIn ? (
+				<section>
+					<h2>Welcome, {user.name}</h2>
+					<p>Your email: {user.email}</p>
+					<img className="w-25" src={user.photoURL} alt="userPhoto" />
+				</section>
+			) : (
+				false
+			)}
+			<br />
+			<section className="my-2">
+				<AuthRegister user={user} setUser={setUser} loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
+			</section>
+		</section>
 	);
 };
 
