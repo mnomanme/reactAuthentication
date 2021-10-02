e8b1d8e57f7e0fe40928febe60d3b9a5618bf721
nomanme <abdullah.noman083@gmail.com>
2021-10-02 11:48:57 +0600

    shipment done

--- 43-shared-auth-and-protected-route/ema-john-auth/src/components/Shipment/Shipment.js
+++ 43-shared-auth-and-protected-route/ema-john-auth/src/components/Shipment/Shipment.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
 const Shipment = () => {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm();
+	const onSubmit = (data) => console.log(data);
+
+	console.log(watch('example'));
+
 	return (
-		<Container>
-			<h1>This is Shipment</h1>
-		</Container>
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<input defaultValue="test" {...register('example')} />
+
+			<input {...register('exampleRequired', { required: true })} />
+			{errors.exampleRequired && <span>This field is required</span>}
+
+			<input type="submit" />
+		</form>
 	);
 };
 
