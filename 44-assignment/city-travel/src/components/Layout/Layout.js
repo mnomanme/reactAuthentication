4ed3f7bc381f247c72b2e0ac9062edb34e329037
nomanme <abdullah.noman083@gmail.com>
2021-10-02 20:20:04 +0600

    routing

--- 44-assignment/city-travel/src/components/Layout/Layout.js
+++ 44-assignment/city-travel/src/components/Layout/Layout.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Blog from '../Blog/Blog';
+import Contact from '../Contact/Contact';
+import Destination from '../Destination/Destination';
+import Header from '../Header/Header';
 import Home from '../Home/Home';
+import Login from '../Login/Login';
+import { NotMatch } from '../NotMatch/NotMatch';
 
 const Layout = () => {
 	return (
-		<>
-			<Home />
-		</>
+		<Router>
+			<Header />
+			<Switch>
+				<Route exact path="/home">
+					<Home />
+				</Route>
+				<Route path="/destination">
+					<Destination />
+				</Route>
+				<Route path="/blog">
+					<Blog />
+				</Route>
+				<Route path="/contact">
+					<Contact />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route exact path="*">
+					<NotMatch />
+				</Route>
+			</Switch>
+		</Router>
 	);
 };
 
