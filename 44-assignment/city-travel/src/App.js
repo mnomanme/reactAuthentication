11be8fc2598724338fc7cf7003821755629435d3
nomanme <abdullah.noman083@gmail.com>
2021-10-02 19:12:04 +0600

    pass layout

--- 44-assignment/city-travel/src/App.js
+++ 44-assignment/city-travel/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import Layout from './components/Layout/Layout';
 
 const App = () => {
 	return (
 		<>
 			<h1>City Travel</h1>
+			<Layout />
 		</>
 	);
 };
