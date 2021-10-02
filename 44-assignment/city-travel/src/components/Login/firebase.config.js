f19b8c70cdc9b27359e44092b0a9f1e3dacaddc3
nomanme <abdullah.noman083@gmail.com>
2021-10-02 21:24:11 +0600

    firebase config okay

---
+++ 44-assignment/city-travel/src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyA5XNr-6hZhvvFtrPi4ZTvacrdXaBXVuy8',
+	authDomain: 'travel-ride.firebaseapp.com',
+	projectId: 'travel-ride',
+	storageBucket: 'travel-ride.appspot.com',
+	messagingSenderId: '664272949514',
+	appId: '1:664272949514:web:aac77aeef502cc096eeaf3',
+};
+
+export default firebaseConfig;
