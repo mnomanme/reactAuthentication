9b6ea729bb62764e91cfc320d2a9fbe1c770cc34
nomanme <abdullah.noman083@gmail.com>
2021-10-02 15:45:25 +0600

    show sign in user name

--- 43.5-firebase-auth-recap/burj-al-arab/src/App.js
+++ 43.5-firebase-auth-recap/burj-al-arab/src/App.js
@@ -5,25 +5,32 @@ import Login from './components/Login/Login';
 import Book from './components/Book/Book';
 import Header from './components/Header/Header';
 
+export const LoginContext = createContext();
+
 const App = () => {
+	const [loggedInUser, setLoggedInUser] = useState({});
+
 	return (
-		<Router>
-			<Header />
-			<Switch>
-				<Route path="/home">
-					<Home />
-				</Route>
-				<Route path="/login">
-					<Login />
-				</Route>
-				<Route path="/book/:bedType">
-					<Book />
-				</Route>
-				<Route exact path="/">
-					<Home />
-				</Route>
-			</Switch>
-		</Router>
+		<LoginContext.Provider value={[loggedInUser, setLoggedInUser]}>
+			<p className="text-center text-info">{loggedInUser.name}</p>
+			<Router>
+				<Header />
+				<Switch>
+					<Route path="/home">
+						<Home />
+					</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/book/:bedType">
+						<Book />
+					</Route>
+					<Route exact path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</Router>
+		</LoginContext.Provider>
 	);
 };
 
--- 43.5-firebase-auth-recap/burj-al-arab/src/components/Login/Login.js
+++ 43.5-firebase-auth-recap/burj-al-arab/src/components/Login/Login.js
@@ -3,33 +3,34 @@ import { Button, Container } from 'react-bootstrap';
 import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import firebaseConfig from './firebase.config';
+import { LoginContext } from '../../App';
 
 const Login = () => {
+	const [loggedInUser, setLoggedInUser] = useContext(LoginContext);
+	const app = initializeApp(firebaseConfig);
+
 	const handleGoogleSignIn = () => {
 		console.log('sign in google');
 
-		const app = initializeApp(firebaseConfig);
 		const googleProvider = new GoogleAuthProvider();
 
 		const auth = getAuth();
 		signInWithPopup(auth, googleProvider)
 			.then((result) => {
-				const credential = GoogleAuthProvider.credentialFromResult(result);
-				const token = credential.accessToken;
-				const user = result.user;
-				console.log(user);
+				const { displayName, email } = result.user;
+				const signInUser = { name: displayName, email: email };
+				setLoggedInUser(signInUser);
+				console.log(signInUser);
 			})
 			.catch((error) => {
 				const errorCode = error.code;
 				const errorMessage = error.message;
-				const email = error.email;
-				const credential = GoogleAuthProvider.credentialFromError(error);
+				console.log(errorCode, errorMessage);
 			});
 	};
 
 	return (
-		<Container className="text-center">
-			<h1>This is Login</h1>
+		<Container className="text-center my-3">
 			<Button onClick={handleGoogleSignIn} variant="outline-secondary">
 				Google Sign In
 			</Button>
