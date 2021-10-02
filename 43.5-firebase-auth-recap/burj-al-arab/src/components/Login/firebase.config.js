5323ef1e952e32b6295dbe8ef93997ef50388d4b
nomanme <abdullah.noman083@gmail.com>
2021-10-02 14:27:55 +0600

    setup & firebase config

---
+++ 43.5-firebase-auth-recap/burj-al-arab/src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyDI9cHcNf1-utb8efPjPLA4XxJoz4e-aXQ',
+	authDomain: 'burj-arab-auth.firebaseapp.com',
+	projectId: 'burj-arab-auth',
+	storageBucket: 'burj-arab-auth.appspot.com',
+	messagingSenderId: '707699505522',
+	appId: '1:707699505522:web:23d0e2d39184f152a085eb',
+};
+
+export default firebaseConfig;
