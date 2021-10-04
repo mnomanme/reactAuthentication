b69460ee96989c8e6629bdcd47c491533796a485
nomanme <abdullah.noman083@gmail.com>
2021-10-04 21:21:59 +0600

    header done

--- 44-assignment/city-travel/src/components/Header/Header.js
+++ 44-assignment/city-travel/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { getAuth, signOut } from '@firebase/auth';
 import React, { useContext } from 'react';
 import { Container, Button, Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -7,25 +8,101 @@ import './Header.css';
 const Header = () => {
 	const [loggedInUser, setLoggedInUser] = useContext(userContext);
 
+	const handleLogOut = () => {
+		const auth = getAuth();
+		signOut(auth)
+			.then(() => {
+				const signOutUser = {
+					isSignedIn: false,
+					name: '',
+					email: '',
+					photo: '',
+					error: '',
+					success: false,
+				};
+				setLoggedInUser(signOutUser);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
 	return (
-		<Container className="header">
-			<nav>
-				<Link to="/home">Home</Link>
-				<Link to="/destination">Destination</Link>
-				<Link to="/blog">Blog</Link>
-				<Link to="/contact">Contact</Link>
-				{loggedInUser?.email ? (
-					<Button onClick={() => setLoggedInUser()} variant="outline-danger " size="lg">
-						Log Out
-					</Button>
-				) : (
-					<Link to="/login">
-						<Button variant="outline-success" size="lg">
-							Log In
-						</Button>
+		<Container className="">
+			{/* <Navbar bg="light" expand="lg">
+				<Navbar.Brand>
+					<Link to="/">
+						<h3>City Ride</h3>
 					</Link>
-				)}
-			</nav>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ml-auto">
+						<Link className="link" to="/">
+							Home
+						</Link>
+						<Link className="link" to="/destination">
+							Destination
+						</Link>
+						<Link className="link" to="/blog">
+							Blog
+						</Link>
+						<Link className="link" to="/contact">
+							Contact
+						</Link>
+						{loggedInUser.displayName ? (
+							<div>
+								<span className="fw-bold me-3 fs-5">{loggedInUser.displayName}</span>
+								<Button onClick={handleLogOut} variant="danger">
+									LogOut
+								</Button>
+							</div>
+						) : (
+							<Link className="link" to="/login">
+								<Button variant="success">Login</Button>
+							</Link>
+						)}
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar> */}
+
+			{/*  */}
+			<Navbar bg="info" variant="dark">
+				<Container>
+					<Navbar.Brand>
+						<Link to="/">
+							<h3>City Ride</h3>
+						</Link>
+					</Navbar.Brand>
+					<Nav className="ml-auto">
+						<Nav.Link>
+							<Link to="/">Home</Link>
+						</Nav.Link>
+						<Nav.Link>
+							<Link to="/destination">Destination</Link>
+						</Nav.Link>
+						<Nav.Link>
+							<Link to="/blog">Blog</Link>
+						</Nav.Link>
+						<Nav.Link>
+							<Link to="/contact">Contact</Link>
+						</Nav.Link>
+
+						{loggedInUser.displayName ? (
+							<div>
+								<span className="fw-bold me-3 fs-5">{loggedInUser.displayName}</span>
+								<Button onClick={handleLogOut} variant="danger">
+									LogOut
+								</Button>
+							</div>
+						) : (
+							<Link className="link" to="/login">
+								<Button variant="success">Login</Button>
+							</Link>
+						)}
+					</Nav>
+				</Container>
+			</Navbar>
 		</Container>
 	);
 };
