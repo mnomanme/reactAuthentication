bc71b0f29eac1dcc5beb5d1140342ff310ae0123
nomanme <abdullah.noman083@gmail.com>
2021-10-05 22:20:22 +0600

    direction solved

--- 44-assignment/city-travel/src/components/Direction/Direction.js
+++ 44-assignment/city-travel/src/components/Direction/Direction.js
@@ -6,13 +6,15 @@ const Direction = (props) => {
 
 	return (
 		<Container>
-			<form onSubmit={handleDirectionRoute} className="">
-				<h5>Pick From</h5>
-				<input required type="text" name="origin" placeholder="From where" className="form-control mt-3 mb-3" />
-				<h5>Pick To</h5>
-				<input required type="text" name="destination" placeholder="Going to" className="form-control mt-3 mb-3" />
-				<input type="submit" value="Search" className="w-100 btn btn-outline-warning form-control " />
-			</form>
+			<section className="col-sm-4">
+				<form onSubmit={handleDirectionRoute}>
+					<h5>Pick From</h5>
+					<input required type="text" name="origin" placeholder="From where" className="form-control mt-3 mb-3" />
+					<h5>Pick To</h5>
+					<input required type="text" name="destination" placeholder="Going to" className="form-control mt-3 mb-3" />
+					<input type="submit" value="Search" className="w-100 btn btn-outline-warning form-control " />
+				</form>
+			</section>
 		</Container>
 	);
 };
